889593babf410acf7036805dfd9ce65852111b96
rjun <rjun0729@gmail.com>
2023-06-15 03:37:05 +0800

    feat: update RuleController and rule-view

--- src/main/java/com/evasionera/controllers/RuleController.java
+++ src/main/java/com/evasionera/controllers/RuleController.java
@@ -1,18 +1,11 @@
 package com.evasionera.controllers;
 
-import com.evasionera.EvasionEraApplication;
 import javafx.fxml.FXML;
 
-public class RuleController {
-
-    private EvasionEraApplication Main;
-
-    public void setMain(EvasionEraApplication Main) {
-        this.Main = Main;
-    }
+public class RuleController extends BaseController{
 
     @FXML
     public void switchToHomeView() {
-        Main.switchToHomeView();
+        main.switchToScene("home");
     }
 }
