9c2f5a62483d4a8991a08bad8f0f5bd14bb7462b
rjun <rjun0729@gmail.com>
2023-06-16 16:00:29 +0800

    refactor: remove unused code and lines

--- src/main/java/com/evasionera/controllers/RuleController.java
+++ src/main/java/com/evasionera/controllers/RuleController.java
@@ -3,9 +3,9 @@ package com.evasionera.controllers;
 import javafx.fxml.FXML;
 
 public class RuleController extends BaseController {
-
     @FXML
     public void switchToHomeView() {
         main.switchToScene("home");
     }
 }
+
